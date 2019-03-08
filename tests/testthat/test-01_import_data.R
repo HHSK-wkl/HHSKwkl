0372cdd85693cee3891a52c13442ed851f4d5160
Johan van Tent <tentvanjohan@hotmail.com>
2019-03-08 08:23:26 +0100

    meer tests toegevoegd

--- tests/testthat/test-01_import_data.R
+++ tests/testthat/test-01_import_data.R
@@ -2,6 +2,7 @@ context("test-01_import_data")
 
 fys_chem <- import_fys_chem(fys_chem_csv = system.file("extdata", "test_fys_chem.csv", package = "HHSKwkl"))
 meetpunten <- import_meetpunten(meetpunten_csv = system.file("extdata", "test_meetpunten.csv", package = "HHSKwkl"))
+parameters <- import_parameters(parameter_csv = system.file("extdata", "test_parameters.csv", package = "HHSKwkl"))
 
 test_that("import_fys_chem works",{
   
@@ -18,6 +19,9 @@ test_that("import_fys_chem works",{
   
   expect_equal(fys_chem$mp[1], "00007")
   expect_equal(fys_chem$datum[1], as.Date("2010-01-19", format = "%Y-%m-%d"))
+  
+  alle_benodigde_fys_chem_kolommen <- all(c("mp", "parnr", "datum", "waarde", "detectiegrens") %in% names(fys_chem))
+  expect_true(alle_benodigde_fys_chem_kolommen)
 
 })
 
@@ -26,4 +30,19 @@ test_that("import_meetpunten works", {
   expect_is(meetpunten$mp, "character")
   expect_is(meetpunten$x, "numeric")
   expect_is(meetpunten$y, "numeric")
+  
+  alle_benodigde_meetpuntkolommen <- all(c("mp", "mpomsch", "x", "y") %in% names(meetpunten))
+  expect_true(alle_benodigde_meetpuntkolommen)
+})
+
+test_that("import_parameters works", {
+  expect_is(parameters, "data.frame")
+  expect_is(parameters$parnr, "numeric")
+  expect_is(parameters$par, "character")
+  expect_is(parameters$parnaamlang, "character")
+  expect_is(parameters$eenheid, "character")
+  
+  alle_benodigde_parameterkolommen <- all(c("parnr", "par", "parnaamlang", "eenheid", "aquo_parcode", "aquo_eenheid", "aquo_hoedanigheid", "aquo_compartiment") %in% names(parameters))
+  expect_true(alle_benodigde_parameterkolommen)
+  
 })
