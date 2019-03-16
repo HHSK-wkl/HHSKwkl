d11d8b233218bbd958098f7203569b03d4dcd17e
Johan van Tent <tentvanjohan@hotmail.com>
2019-03-16 17:04:06 +0100

    package sf geimplementeerd voor de functie add_lat_long

--- R/02_dataframe_utils.R
+++ R/02_dataframe_utils.R
@@ -133,14 +133,14 @@ randomize <- function(data) {
 
 #' Toevoegen van latidute en longitude
 #' 
-#' De functie voegt de latitude en longitude aan een dataframe op basis van coordinaten. Default is
-#' van RD-stelsel naar WGS84
+#' De functie voegt de latitude (Y) en longitude (X) aan een dataframe op basis van coordinaten. Default is
+#' van RD-stelsel (EPSG = 28992) naar WGS84 (EPSG = 4326)
 #'
 #' @param df Dataframe met coordinaten
-#' @param x_coord Character. Kolom met de x-coordinaat
-#' @param y_coord Character. Kolom met de y-coordinaat
-#' @param source_crs Coordinaten Referentie Systeem van de bron coordinaten. Default is RD-stelsel.
-#' @param goal_crs Coordinaten Referentie Systeem van de doelcoordinaten. Default is WGS84.
+#' @param x_coord Tekst - Kolom met de x-coordinaat
+#' @param y_coord Tekst -  Kolom met de y-coordinaat
+#' @param source_crs Coordinaten Referentie Systeem van de broncoordinaten: als integer met EPSG-code of als proj4string.
+#' @param goal_crs Coordinaten Referentie Systeem van de doelcoordinaten: als integer met EPSG-code of als proj4string.
 #'
 #' @return Geeft hetzelfde dataframe met een kolom long en lat toegevoegd.
 #' @export
@@ -154,45 +154,20 @@ randomize <- function(data) {
 #' 
 #' }
 add_lat_long <- function(df, x_coord = "x", y_coord = "y", 
-                         source_crs = "+init=EPSG:28992", goal_crs = "+init=EPSG:4326"){
+                         source_crs = 28992, goal_crs = 4326){
   
-  df %>% 
-    dplyr::mutate(latlong = purrr::map2(.[[x_coord]], .[[y_coord]], coordinate_conversion, 
-                                 source_crs = source_crs, goal_crs = goal_crs)) %>% 
-    tidyr::unnest()
+  long_lat <- 
+    df %>% 
+    sf::st_as_sf(coords = c(x_coord, y_coord), remove = FALSE, na.fail = FALSE, crs = source_crs) %>% 
+    sf::st_transform(crs = goal_crs) %>%  
+    sf::st_coordinates() %>% 
+    tibble::as_tibble() %>% 
+    dplyr::rename(long = X, lat = Y) 
+  
+  dplyr::bind_cols(df, long_lat)  
 }
 
   
-#' Coordinate Conversion
-#' 
-#' Helper functie voor coordinaten conversie. Deze functie wordt gebruikt door add_lat_long
-#'
-#' @param x x-coordinaat van de bron
-#' @param y y-coordinaat van de bron
-#' @param source_crs Coordinaten Referentie Systeem van de bron coordinaten. Default is RD-stelsel.
-#' @param goal_crs Coordinaten Referentie Systeem van de doelcoordinaten. Default is WGS84.
-#'
-#' @return Een dataframe met een long en een lat
-#' 
-#'
-#' @examples
-#' \dontrun{
-#' 
-#' coordinate_conversion(x = 111111, y = 444444)
-#' }
-coordinate_conversion <- function(x, y, source_crs = "+init=EPSG:28992", goal_crs = "+init=EPSG:4326"){
-  
-  if ( any(is.na(c(x,y))) ) {return(tibble::tibble(long = NA, lat = NA))}
-  
-  temp <- data.frame(x,y)
-  sp::coordinates(temp) <- ~x+y
-  sp::proj4string(temp) <- sp::CRS(source_crs)
-  transformed <- as.data.frame(sp::spTransform(temp, goal_crs))
-  
-  tibble::tibble(long = transformed[1,1], lat = transformed[1,2])
-  
-}  
-
 
 # my_c --------------------------------------------------------------------
 
