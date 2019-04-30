6ad72429740d335395c38f22fb9c6994dd671d6d
Johan van Tent <tentvanjohan@hotmail.com>
2019-04-30 21:01:39 +0200

    Opzoekfuncties voor TWN geimplementeerd.

--- R/50_biologie_utils.R
+++ R/50_biologie_utils.R
@@ -1,6 +1,8 @@
 ### These functions depend on the package autecodata which can be found on
 ### https://github.com/RedTent/autecodata
 
+# Check autecodata --------------------------------------------------------
+
 #' Check autecodata
 #'
 #' Deze functie checkt of autecodata is geinstalleerd en geeft een foutmelding
@@ -14,10 +16,12 @@ check_autecodata <- function(){
          Dit package kan worden gedownload op https://github.com/RedTent/autecodata")}}
 
 
+# Toevoegen TWN-voorkeurnamen ---------------------------------------------
+
 #' Toevoegen TWN-voorkeurnamen
 #'
 #' Deze functie voegt op basis van de TWN-lijst de voorkeurnamen toe aan een
-#' dataframe. Deze functie maakt gebruik van de dataset [autecodata::twn_lijst]
+#' dataframe. Deze functie maakt gebruik van de dataset twn_lijst uit het package autecodata.
 #'
 #' @param df Een dataframe met biologische data
 #' @param naam_orig Naam van de kolom met originele namen. Dit argument is
@@ -36,8 +40,10 @@ check_autecodata <- function(){
 #' @return Een dataframe met daaraan de voorkeurnamen toegevoegd.
 #' @export
 #'
-#' @examples
+#' @seealso 
+#' [autecodata::twn_lijst]
 #'
+#' @examples
 #' \dontrun{
 #'
 #' bio %>% add_twn_voorkeurnaam(naam_orig = naam, naam_doel = naam_voorkeur)
@@ -46,15 +52,38 @@ check_autecodata <- function(){
 #' 
 add_twn_voorkeurnaam <- function(df, naam_orig = naam, naam_doel = naam_voorkeur){
   check_autecodata()
-
+  
   naam_orig <- dplyr::enquo(naam_orig)
   naam_doel <- dplyr::enquo(naam_doel)
   
-  twn_voorkeurnaam <- autecodata::twn_lijst %>% 
-                      dplyr::mutate(refername = ifelse(is.na(refername), taxonname, refername)) %>% 
-                      maak_opzoeker(key = taxonname, value = refername)
-  
-  df <- df %>% dplyr::mutate(!!naam_doel := twn_voorkeurnaam(!!naam_orig))
+  df <- df %>% dplyr::mutate(!!naam_doel := get_twn_voorkeur(!!naam_orig))
   df
 }
 
+
+
+
+
+# Find  parents ----------------------------------------------------
+
+
+
+
+
+
+
+
+
+# find_matching_parent <- function(naam_bron, namen_doel){
+#   check_autecodata()
+#   
+#   if (naam_bron %in% namen_doel) {return(naam_bron)}
+#   
+#   
+#   temp_naam <- naam_bron
+#   
+#   # deze blijft nodig om eindeloos aanmaken van de opzoektabel te voorkomen
+#   zoek_parent <- autecodata::twn_lijst %>% maak_opzoeker(key = taxonname, value = parentname) 
+#     
+#   while(!(temp_naam %in% namen_doel) & !is.na(temp_naam) ){}
+# }
---
+++ R/51_get_twn_info.R
@@ -0,0 +1,118 @@
+## These functions depend on the package autecodata which can be found on
+## https://github.com/RedTent/autecodata
+
+
+
+#' Opzoeken van de TWN informatie
+#'
+#' Deze functies zoeken bij taxonnamen de bijbehorende informatie uit de
+#' TWN-lijst.
+#'
+#' @details Deze functies accepteren een vector met taxonnamen en retourneren de
+#'   betreffende informatie uit de TWN-lijst. Als de taxonnaam niet in de
+#'   TWN-lijst voorkomt retourneert de functie `NA`
+#'
+#'   - `get_twn_voorkeur`: Geeft de voorkeurnaam van het taxon. Als het al de
+#'   voorkeurnaam is wordt de originele naam teruggegeven.
+#'   - `get_twn_parent`: Geeft de parent van het taxon. 
+#'   - `get_twn_status`: Geeft de status van het taxon. 
+#'   - `get_twn_localname`: Geeft de Nederlandse naam van het taxon. 
+#'   - `get_twn_taxonlevel`: Geeft het taxonlevel van het taxon.
+#'
+#' @param namen Een vector met taxonnamen
+#'
+#' @return Een vector met TWN-informatie. Namen die niet in de TWN worden
+#'   gevonden krijgen `NA`.
+#'   
+#' @note De TWN-lijst is niet altijd volledig consistent. Diverse taxa staan
+#'   meerdere malen in de lijst. In dat geval wordt het eerste resultaat
+#'   geretourneerd.
+#'   
+#'   Als een taxon niet de voorkeurnaam is dan ontbreekt er vaak informatie 
+#'   zoals de parent van het taxon
+#'
+#' @examples
+#'
+#' get_twn_voorkeur(c("Bufo calamita", "Bufo"))
+#' get_twn_parent(c("Bufo calamita", "Bufo"))
+#' get_twn_status(c("Bufo calamita", "Bufo"))
+#' get_twn_localname(c("Bufo calamita", "Bufo"))
+#' get_twn_taxonlevel(c("Bufo calamita", "Bufo"))
+#'
+#' @name get_twn_info
+#' @seealso [autecodata::twn_lijst]
+#'   
+NULL
+
+#' @export
+#' @rdname get_twn_info
+get_twn_voorkeur <- function(namen) {
+  check_autecodata()  
+
+  opzoektabel <- autecodata::twn_lijst %>% 
+    dplyr::mutate(refername = ifelse(is.na(refername), taxonname, refername)) %>% 
+    dplyr::select(taxonname, refername) %>% 
+    dplyr::distinct() %>% 
+    tibble::deframe()
+  
+  unname(opzoektabel[as.character(namen)])
+}
+
+#' @export
+#' @rdname get_twn_info
+get_twn_parent <- function(namen) {
+  check_autecodata()
+  
+  opzoektabel <- autecodata::twn_lijst %>% 
+    dplyr::select(taxonname, parentname) %>% 
+    dplyr::distinct() %>% 
+    tibble::deframe()
+  
+  unname(opzoektabel[as.character(namen)])
+  
+} 
+
+#' @export
+#' @rdname get_twn_info
+get_twn_status <- function(namen) {
+  check_autecodata()
+  
+  opzoektabel <- autecodata::twn_lijst %>% 
+    dplyr::select(taxonname, status) %>% 
+    dplyr::distinct() %>% 
+    tibble::deframe()
+  
+  unname(opzoektabel[as.character(namen)])
+  
+}
+
+#' @export
+#' @rdname get_twn_info
+get_twn_localname <- function(namen) {
+  check_autecodata()
+  
+  opzoektabel <- autecodata::twn_lijst %>% 
+    dplyr::select(taxonname, localname) %>% 
+    dplyr::distinct() %>% 
+    tibble::deframe()
+  
+  unname(opzoektabel[as.character(namen)])
+  
+}
+
+#' @export
+#' @rdname get_twn_info
+get_twn_taxonlevel <- function(namen) {
+  check_autecodata()
+  
+  opzoektabel <- autecodata::twn_lijst %>% 
+    dplyr::select(taxonname, taxonlevel) %>% 
+    dplyr::distinct() %>% 
+    tibble::deframe()
+  
+  unname(opzoektabel[as.character(namen)])
+  
+}
+
+
+
