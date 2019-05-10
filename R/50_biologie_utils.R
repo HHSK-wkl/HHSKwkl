88dff79df296ad78d2981152bf855716d4eb6c39
Johan van Tent <tentvanjohan@hotmail.com>
2019-05-10 16:07:52 +0200

    edited documentation

--- R/50_biologie_utils.R
+++ R/50_biologie_utils.R
@@ -6,14 +6,15 @@
 #' Check autecodata
 #'
 #' Deze functie checkt of autecodata is geinstalleerd en geeft een foutmelding
-#' als dat niet zo is
+#' als dat niet zo is. Dit is een interne functie.
 #'
 #' @return Niets of een foutmelding als auteocdata niet is geinstalleerd.
 #'
 check_autecodata <- function(){
   if (!requireNamespace("autecodata", quietly = TRUE)) {
-    stop("Deze functie werkt alleen als het package autecodata is geinstalleerd.
-         Dit package kan worden gedownload op https://github.com/RedTent/autecodata")}}
+    stop('Deze functie werkt alleen als het package autecodata is geinstalleerd.
+         Dit package kan worden gedownload op https://github.com/RedTent/autecodata 
+         of direct worden geinstalleerd met `devtools::install_github("RedTent/autecodata")`')}}
 
 
 # Toevoegen TWN-voorkeurnamen ---------------------------------------------
@@ -21,7 +22,11 @@ check_autecodata <- function(){
 #' Toevoegen TWN-voorkeurnamen
 #'
 #' Deze functie voegt op basis van de TWN-lijst de voorkeurnamen toe aan een
-#' dataframe. Deze functie maakt gebruik van de dataset twn_lijst uit het package autecodata.
+#' dataframe. Deze functie maakt gebruik van de dataset twn_lijst uit het
+#' package autecodata.
+#'
+#' @details Deze functie maakt gebruik van de functie [get_twn_voorkeur] om de
+#'   voorkeurnaam op te zoeken indien deze beschikbaar is.
 #'
 #' @param df Een dataframe met biologische data
 #' @param naam_orig Naam van de kolom met originele namen. Dit argument is
@@ -31,17 +36,16 @@ check_autecodata <- function(){
 #'
 #' @note
 #'
-#' Deze functie is afhankelijk van de beschikbaarheid van de package autecodata. Dit kan gedownload worden op [https://github.com/RedTent/autecodata](https://github.com/RedTent/autecodata)
+#' Deze functie is afhankelijk van de beschikbaarheid van de package autecodata.
+#' Dit kan gedownload worden op
+#' [https://github.com/RedTent/autecodata](https://github.com/RedTent/autecodata)
 #'
 #' Bij een ongeldige TWN-naam geeft deze functie een `NA`.
-#'
-#' De namen zijn hoofdlettergevoelig.
 #'
 #' @return Een dataframe met daaraan de voorkeurnamen toegevoegd.
 #' @export
 #'
-#' @seealso 
-#' [autecodata::twn_lijst]
+#' @seealso [autecodata::twn_lijst]
 #'
 #' @examples
 #' \dontrun{
@@ -51,7 +55,7 @@ check_autecodata <- function(){
 #' }
 #' 
 add_twn_voorkeurnaam <- function(df, naam_orig = naam, naam_doel = naam_voorkeur){
-  check_autecodata()
+  #check_autecodata() # zit al in get_twn_voorkeur en is hier overbodig geworden
   
   naam_orig <- dplyr::enquo(naam_orig)
   naam_doel <- dplyr::enquo(naam_doel)
@@ -66,24 +70,67 @@ add_twn_voorkeurnaam <- function(df, naam_orig = naam, naam_doel = naam_voorkeur
 
 # Find  parents ----------------------------------------------------
 
-
-
-
-
-
-
-
-
-# find_matching_parent <- function(naam_bron, namen_doel){
+# add_matching_parent <- function(df, naam_orig, namen_match, naam_doel = naam_matching_parent){
 #   check_autecodata()
 #   
-#   if (naam_bron %in% namen_doel) {return(naam_bron)}
+#   if (!all(namen_match %in% autecodata::twn_lijst$taxonname)) {warning("Niet alle gezochte soorten komen voor in de TWN-lijst")}
 #   
+#   df %>% dplyr::mutate(tempnaam)
 #   
-#   temp_naam <- naam_bron
-#   
-#   # deze blijft nodig om eindeloos aanmaken van de opzoektabel te voorkomen
-#   zoek_parent <- autecodata::twn_lijst %>% maak_opzoeker(key = taxonname, value = parentname) 
-#     
-#   while(!(temp_naam %in% namen_doel) & !is.na(temp_naam) ){}
 # }
+
+
+# find_matching_parent <- function(naam_bron, namen_match){
+#   check_autecodata()
+# 
+#   if (naam_bron %in% namen_doel) {return(naam_bron)}
+#   zoek_voorkkeur
+#   
+#   temp_naam <- get_twn_voorkeur(naam_bron)
+# 
+#   # deze blijft nodig om eindeloos aanmaken van de opzoektabel te voorkomen
+#   zoek_parent <- autecodata::twn_lijst %>% maak_opzoeker(key = taxonname, value = parentname)
+#   
+# 
+#   while (!(temp_naam %in% namen_doel) & !is.na(temp_naam) ) {
+#     temp_naam <- zoek_parent(temp_naam)
+# 
+#   }
+#   temp_naam
+# }
+
+# add_matching_parent <- function(df, naam_orig = naam, namen_match, naam_doel = naam_matching_parent){
+#   check_autecodata()
+# 
+# 
+# # Voorwerk ----------------------------------------------------------------
+#                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+#   naam_orig <- dplyr::enquo(naam_orig)
+#   naam_doel <- dplyr::enquo(naam_doel)
+#   
+#   if (!all(namen_match %in% autecodata::twn_lijst$taxonname)) {warning("Niet alle gezochte soorten komen voor in de TWN-lijst")}
+#   if (any(is.na(namen_match))) {
+#     namen_match <- namen_match[!is.na(namen_match)] 
+#     message("NA's verwijderd uit namen_match")
+#     }
+#   
+#   if (all(df$naam_orig %in% namen_match)) return(mutate(df, !!naam_doel := !!naam_orig))
+# 
+#   opzoeker_voorkeur <- 
+#     autecodata::twn_lijst %>% 
+#     dplyr::mutate(refername = ifelse(is.na(refername), taxonname, refername)) %>% 
+#     maak_opzoeker(key = taxonname, value = refername)
+#   
+#   opzoeker_parent <- autecodata::twn_lijst %>% maak_opzoeker(key = taxonname, value = parentname)
+#   
+#   no_match <- function(naam) {!(naam %in% namen_match)}
+# 
+# 
+# # Het echte werk ----------------------------------------------------------
+# 
+#   df_temp <- df %>% dplyr::mutate(tempnaam1 = !!naam_orig, 
+#                                   tempnaam2 = ifelse(no_match(tempnaam1), opzoeker_voorkeur(tempnaam2), tempnaam1))
+#   
+# }
+
+
