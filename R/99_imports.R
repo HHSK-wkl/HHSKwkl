7aa89dbdeb5171b61592822c8ddde324ceb38be2
Johan van Tent <tentvanjohan@hotmail.com>
2019-05-15 15:16:57 +0200

    added a few tweeks to get rid of no visible binding for global variable

--- R/99_imports.R
+++ R/99_imports.R
@@ -1,7 +1,8 @@
-#'
 #' @importFrom dplyr %>% 
-#' @importFrom rlang !! :=
+#' @importFrom rlang !! := .data
 #' @importFrom  grid grid.raster grid.polygon grid.lines grid.points grid.text gpar unit
 #'
 #'
 NULL
+
+
---
+++ R/99_package_description.R
@@ -0,0 +1,64 @@
+#' HHSKwkl: Een package voor waterkwaliteit
+#'
+#' Functies voor waterkwaliteitsanalyse. Zie verder: [https://redtent.github.io/HHSKwkl/](https://redtent.github.io/HHSKwkl/)
+#' 
+#'
+#' @docType package
+#' @name HHSKwkl
+NULL
+
+
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")  utils::globalVariables(c(".",
+                                                         "Aquo-code",
+                                                         "Compartiment",
+                                                         "Eenheid",
+                                                         "JGM",
+                                                         "MAX",
+                                                         "Norm.code",
+                                                         "P90",
+                                                         "Waarde",
+                                                         "Waardebewerkingsmethode.code",
+                                                         "X",
+                                                         "Y",
+                                                         "aangetroffen",
+                                                         "aantal",
+                                                         "aantal_det",
+                                                         "aantal_detectiegrens",
+                                                         "alleen_detectiegrens",
+                                                         "datum",
+                                                         "detectiegrens",
+                                                         "eenheid",
+                                                         "factor_JGM",
+                                                         "factor_MAX",
+                                                         "factor_P90",
+                                                         "hhskblauw",
+                                                         "hhskgroen",
+                                                         "hoogste_overschrijding",
+                                                         "jaar",
+                                                         "localname",
+                                                         "logo",
+                                                         "methode",
+                                                         "min_is_max",
+                                                         "mp",
+                                                         "mpomsch",
+                                                         "n",
+                                                         "naam",
+                                                         "naam_voorkeur",
+                                                         "norm_JGM",
+                                                         "norm_MAX",
+                                                         "norm_P90",
+                                                         "par",
+                                                         "parentname",
+                                                         "parnaamlang",
+                                                         "parnr",
+                                                         "refername",
+                                                         "schonevoeten",
+                                                         "status",
+                                                         "taxonlevel",
+                                                         "taxonname",
+                                                         "waarde",
+                                                         "waarde_det_half"
+                                                         ))
