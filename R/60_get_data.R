1e00c1114170d81e815efa9b70410382b2557223
Johan van Tent <tentvanjohan@hotmail.com>
2025-08-01 08:54:26 +0200

    ws_grens_rd en ws_grens_wgs opgenomen als object ipv als functie

--- R/60_get_data.R
+++ R/60_get_data.R
@@ -5,38 +5,17 @@
 #' 
 #' Waterschapsgrens als dataframe of als bestand
 #' 
-#' @param crs optionele CRS-code als getal voor conversie van de CRS
 #' @param pad een string voor het pad op de ws_grens.gpkg op te slaan. Default is de map *data*.
 #'
-#' @return `ws_grens()` geeft een sf dataframe met de waterschapsgrens. 
-#' 
-#' `get_ws_grens()` slaat ws_grens.gpkg op de gekozen locatie.
-#'
-#' @name ws_grens
+#' @return `get_ws_grens()` slaat ws_grens.gpkg op de gekozen locatie.
 #'
 #' @examples
 #' 
 #' \dontrun{
-#' ws_grens(crs = 4326)
+#' get_ws_grens("data")
+#' }
 #' 
-#' get_ws_grens("data")}
-#' 
-
-
 #' @export
-#' @rdname ws_grens
-ws_grens <- function(crs = NULL) {
-  bestandsnaam <- system.file("extdata/ws_grens.gpkg", package = "HHSKwkl")
-  ws_grens <- sf::st_read(bestandsnaam, crs = 28992, quiet = TRUE)
-  
-  if (!is.null(crs)) ws_grens <- sf::st_transform(ws_grens, crs)
-  
-  ws_grens
-  
-}
-
-#' @export
-#' @rdname ws_grens
 get_ws_grens <- function(pad = "data"){
   bestandsnaam <- system.file("extdata/ws_grens.gpkg", package = "HHSKwkl")
   file.copy(from = bestandsnaam, to = pad, overwrite = TRUE)
