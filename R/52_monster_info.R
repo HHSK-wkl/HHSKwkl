d5aa038edd71a1154cf5bc6945bf74f3a77391a4
Johan van Tent <tentvanjohan@hotmail.com>
2025-08-01 10:28:49 +0200

    Aangepaste opmerking

--- R/52_monster_info.R
+++ R/52_monster_info.R
@@ -1,29 +1,10 @@
-# monsterdiagnostiek - compleetheid, duplicaten, twnconformiteit
-# monsterbeschrijving - aantal soorten, geslachten e.d 
-# monsterverrijking -
-# diversiteitsindexen,?beoordelingen? omgevingsfactoren etc.
-#
 # basisfuncties die uitgaan van  1 monster. Overkoepelende functies met group_by
 # en een verzameling basisfuncties
+# 
+# Het is de vraag in hoeverre het zinvol is om deze functies verder te ontwikkelen.
 
 
 # Monster-summaryfunctie --------------------------------------------------
-
-
-# mon_summary_old <- function(df) {
-#   summarise(df, 
-#             taxa_missing   = mon_taxa_missing(naam),
-#             taxa_n         = mon_taxa_n(naam),
-#             taxa_n_uniek   = mon_taxa_n_uniek(naam),
-#             taxa_n_dubbel  = mon_taxa_n_dubbel(naam),
-#             waarde_missing = mon_waarde_missing(waarde),
-#             waarde_nul     = mon_waarde_nul(waarde),
-#             waarde_tot     = mon_waarde_tot(waarde),
-#             waarde_max     = mon_waarde_max(waarde),
-#             twn_niet_in_twn  = mon_twn_niet_in_twn(naam),
-#             twn_status_afwijkend = mon_twn_status_afwijkend(naam)
-#             )
-# }
 
 #' Samenvatting monster
 #'
