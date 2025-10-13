873c9d3c4eeecb7b9cc767f74bdf4e2e01dfb376
Johan van Tent <tentvanjohan@hotmail.com>
2025-10-13 14:33:12 +0200

    nieuwe thema's

--- R/31_ggplot_themas.R
+++ R/31_ggplot_themas.R
@@ -24,108 +24,14 @@
 #' 
 NULL
 
-#' @rdname hhskthema
-#' @export
 hhskthema <- function(){
-  #require(ggplot2)
-  hhskthema <- ggplot2::theme_light() + ggplot2::theme(
-    
-    plot.title =    ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
-    plot.subtitle = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0, size = ggplot2::rel(1.1)),
-    plot.caption =  ggplot2::element_text(color = "grey40", face = "italic"),
-    
-    axis.title =  ggplot2::element_text(color = "grey40"),
-    axis.text =   ggplot2::element_text(color = "grey40"),
-    axis.ticks =  ggplot2::element_line(color = "grey40"),
-    axis.line = ggplot2::element_line(color = "grey40", linewidth = 0.5),
-    
-    panel.border =     ggplot2::element_blank(),#ggplot2::element_rect(color = hhskblauw, size = 1),
-    panel.grid.major = ggplot2::element_line(color = "grey80", linetype = "dotted", linewidth = 0.5),
-    panel.grid.minor = ggplot2::element_blank(),#ggplot2::element_line(color = "grey60", linetype = "dotted", size = 0.5),
-    
-    legend.title = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
-    legend.text =  ggplot2::element_text(color = "grey40"),
-    
-    strip.background = ggplot2::element_rect(fill = NA, colour = "grey50"),
-    strip.text =       ggplot2::element_text(face = "bold", color = "grey50")
-  )
-  hhskthema
-}
-
-#' @rdname hhskthema
-#' @export
-hhskthema_kaart <- function(){
-  #require(ggplot2)
-  hhskthema <- ggplot2::theme_light() + ggplot2::theme(
-    
-    plot.title    = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
-    plot.subtitle = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0, size = ggplot2::rel(1.1)),
-    plot.caption  = ggplot2::element_text(color = "grey40", face = "italic"),
-    plot.title.position = "plot",
-    
-    axis.title =  ggplot2::element_text(color = "grey40"),
-    axis.text  =  ggplot2::element_blank(),
-    axis.ticks =  ggplot2::element_blank(),
-    axis.line  =  ggplot2::element_blank(),
-    
-    panel.border     = ggplot2::element_blank(),
-    panel.grid.major = ggplot2::element_blank(),
-    panel.grid.minor = ggplot2::element_blank(),
-    
-    legend.title = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
-    legend.text  = ggplot2::element_text(color = "grey40"),
-    
-    strip.background = ggplot2::element_rect(fill = NA, colour = "grey50"),
-    strip.text       = ggplot2::element_text(face = "bold", color = "grey50")
-  )
-  hhskthema
-}
-
-#' @rdname hhskthema
-#' @export
-thema_hor_bar <-
-  hhskthema() +
-  ggplot2::theme(axis.ticks.y           = ggplot2::element_blank(),
-                 panel.grid.major.y     = ggplot2::element_blank(),
-                 axis.line.y            = ggplot2::element_blank(),
-                 plot.title.position    = "plot",
-                 plot.caption.position  = "plot",
-                 plot.subtitle          = ggplot2::element_text(face = "italic"))
-
-#' @rdname hhskthema
-#' @export
-thema_vert_bar <-
-  hhskthema() +
-  ggplot2::theme(axis.ticks.x           = ggplot2::element_blank(),
-                 panel.grid.major.x     = ggplot2::element_blank(),
-                 axis.line.x            = ggplot2::element_blank(),
-                 plot.title.position    = "plot",
-                 plot.caption.position  = "plot",
-                 plot.subtitle          = ggplot2::element_text(face = "italic"))
-
-#' @rdname hhskthema
-#' @export
-thema_line_facet <-
-  hhskthema() +
-  ggplot2::theme(panel.grid.major.x    = ggplot2::element_blank(),
-                 plot.title.position   = "plot",
-                 plot.caption.position = "plot",
-                 plot.subtitle         = ggplot2::element_text(face = "italic"),
-                 panel.spacing         = unit(40, "points"),
-                 strip.background      = ggplot2::element_rect(fill = NA, colour = NA),
-                 strip.text            = ggplot2::element_text(face = "bold", color = "grey50", size = 12)
-  )
-
-#' @rdname hhskthema
-#' @export
-thema_test <- function(){
   
   if (check_ruda_aanwezig()) {
     systemfonts::register_variant(
-    name = "Ruda Title",
-    family = "Ruda",
-    weight = "heavy"
-  )
+      name = "Ruda Title",
+      family = "Ruda",
+      weight = "heavy"
+    )
     
     systemfonts::register_variant(
       name = "Ruda Bold",
@@ -156,7 +62,7 @@ thema_test <- function(){
       legend.title = ggplot2::element_text(face = "bold"),
       legend.text =  ggplot2::element_text(size = ggplot2::rel(0.9)),
       
-      strip.background = ggplot2::element_rect(fill = "#E1FAFDFF", colour = NA), # prismatic::clr_lighten(blauw_l_orig, 0.6)
+      strip.background = ggplot2::element_rect(fill = "#EDF5FFFF", colour = NA), # prismatic::clr_lighten(blauw_l, 0.7)
       strip.text =       ggplot2::element_text(family = "Ruda Title", color = blauw_d, size = ggplot2::rel(0.9))
       
     ) |>
@@ -166,6 +72,7 @@ thema_test <- function(){
   
 }
 
+# interne functie
 check_ruda_aanwezig <- function(){
   
   ruda_aanwezig <- 
@@ -183,3 +90,153 @@ check_ruda_aanwezig <- function(){
   invisible(ruda_aanwezig)
   
 }
+
+hhskthema_kaart <- function(){
+  
+  if (check_ruda_aanwezig()) {
+    systemfonts::register_variant(
+      name = "Ruda Title",
+      family = "Ruda",
+      weight = "heavy"
+    )
+    
+    systemfonts::register_variant(
+      name = "Ruda Bold",
+      family = "Ruda",
+      weight = "bold"
+    )
+  }
+  thema <-
+    ggplot2::theme(
+      text = ggplot2::element_text(family = "Ruda Bold", color = "grey50"),
+      title = ggplot2::element_text(family = "Ruda Title"),
+      line = ggplot2::element_line(color = "grey50"),
+      geom = ggplot2::element_geom(color = blauw, fill = blauw_l, accent = oranje),
+      
+      plot.title =    ggplot2::element_text(color = blauw_d,      size = ggplot2::rel(1.5)),
+      plot.subtitle = ggplot2::element_text(family = "Ruda Bold", size = ggplot2::rel(1.1)),
+      plot.caption =  ggplot2::element_text(family = "Ruda",      size = ggplot2::rel(0.9)),
+      
+      axis.title =  ggplot2::element_text(),
+      axis.text  =  ggplot2::element_blank(),
+      axis.ticks =  ggplot2::element_blank(),
+      axis.line  =  ggplot2::element_blank(),
+      
+      panel.border     = ggplot2::element_blank(),
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      
+      legend.title = ggplot2::element_text(face = "bold"),
+      legend.text =  ggplot2::element_text(size = ggplot2::rel(0.9)),
+      
+      strip.background = ggplot2::element_rect(fill = "#EDF5FFFF", colour = NA), # prismatic::clr_lighten(blauw_l, 0.7)
+      strip.text =       ggplot2::element_text(family = "Ruda Title", color = blauw_d, size = ggplot2::rel(0.9))
+      
+    ) |>
+    ggplot2::complete_theme(default = ggplot2::theme_light())
+  
+  thema
+  
+}
+
+
+# Oude thema's ------------------------------------------------------------
+
+
+
+#' @rdname hhskthema
+#' @export
+hhskthema_oud <- function(){
+  #require(ggplot2)
+  hhskthema <- ggplot2::theme_light() + ggplot2::theme(
+
+    plot.title =    ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
+    plot.subtitle = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0, size = ggplot2::rel(1.1)),
+    plot.caption =  ggplot2::element_text(color = "grey40", face = "italic"),
+
+    axis.title =  ggplot2::element_text(color = "grey40"),
+    axis.text =   ggplot2::element_text(color = "grey40"),
+    axis.ticks =  ggplot2::element_line(color = "grey40"),
+    axis.line = ggplot2::element_line(color = "grey40", linewidth = 0.5),
+
+    panel.border =     ggplot2::element_blank(),#ggplot2::element_rect(color = hhskblauw, size = 1),
+    panel.grid.major = ggplot2::element_line(color = "grey80", linetype = "dotted", linewidth = 0.5),
+    panel.grid.minor = ggplot2::element_blank(),#ggplot2::element_line(color = "grey60", linetype = "dotted", size = 0.5),
+
+    legend.title = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
+    legend.text =  ggplot2::element_text(color = "grey40"),
+
+    strip.background = ggplot2::element_rect(fill = NA, colour = "grey50"),
+    strip.text =       ggplot2::element_text(face = "bold", color = "grey50")
+  )
+  hhskthema
+}
+
+#' @rdname hhskthema
+#' @export
+hhskthema_kaart_oud <- function(){
+  #require(ggplot2)
+  hhskthema <- ggplot2::theme_light() + ggplot2::theme(
+
+    plot.title    = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
+    plot.subtitle = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0, size = ggplot2::rel(1.1)),
+    plot.caption  = ggplot2::element_text(color = "grey40", face = "italic"),
+    plot.title.position = "plot",
+
+    axis.title =  ggplot2::element_text(color = "grey40"),
+    axis.text  =  ggplot2::element_blank(),
+    axis.ticks =  ggplot2::element_blank(),
+    axis.line  =  ggplot2::element_blank(),
+
+    panel.border     = ggplot2::element_blank(),
+    panel.grid.major = ggplot2::element_blank(),
+    panel.grid.minor = ggplot2::element_blank(),
+
+    legend.title = ggplot2::element_text(color = "grey50", face = "bold", hjust = 0),
+    legend.text  = ggplot2::element_text(color = "grey40"),
+
+    strip.background = ggplot2::element_rect(fill = NA, colour = "grey50"),
+    strip.text       = ggplot2::element_text(face = "bold", color = "grey50")
+  )
+  hhskthema
+}
+
+
+
+#' #' @rdname hhskthema
+#' #' @export
+#' thema_hor_bar <-
+#'   hhskthema() +
+#'   ggplot2::theme(axis.ticks.y           = ggplot2::element_blank(),
+#'                  panel.grid.major.y     = ggplot2::element_blank(),
+#'                  axis.line.y            = ggplot2::element_blank(),
+#'                  plot.title.position    = "plot",
+#'                  plot.caption.position  = "plot",
+#'                  plot.subtitle          = ggplot2::element_text(face = "italic"))
+
+#' #' @rdname hhskthema
+#' #' @export
+#' thema_vert_bar <-
+#'   hhskthema() +
+#'   ggplot2::theme(axis.ticks.x           = ggplot2::element_blank(),
+#'                  panel.grid.major.x     = ggplot2::element_blank(),
+#'                  axis.line.x            = ggplot2::element_blank(),
+#'                  plot.title.position    = "plot",
+#'                  plot.caption.position  = "plot",
+#'                  plot.subtitle          = ggplot2::element_text(face = "italic"))
+#' 
+#' #' @rdname hhskthema
+#' #' @export
+#' thema_line_facet <-
+#'   hhskthema() +
+#'   ggplot2::theme(panel.grid.major.x    = ggplot2::element_blank(),
+#'                  plot.title.position   = "plot",
+#'                  plot.caption.position = "plot",
+#'                  plot.subtitle         = ggplot2::element_text(face = "italic"),
+#'                  panel.spacing         = unit(40, "points"),
+#'                  strip.background      = ggplot2::element_rect(fill = NA, colour = NA),
+#'                  strip.text            = ggplot2::element_text(face = "bold", color = "grey50", size = 12)
+#'   )
+#' 
+#' #' @rdname hhskthema
+#' #' @export
